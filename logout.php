33d3fa1951cb55fc48dcb1f3ad77c14fe7e64a29
EBAKE <Evan_Baker@baylor.edu>
2018-12-06 16:18:29 -0600

    added cookie reset in logout.php

--- logout.php
+++ logout.php
@@ -2,6 +2,16 @@
 
 //Reset any cookies that were previously set
 setcookie("menubar", 0, time() + 86400, "/");
+setcookie("id", 0, time() + 86400, "/");
+setcookie("acceptStudent", 0, time() + 86400, "/");
+setcookie("invalidBirthday", 0, time() + 86400, "/");
+setcookie("invalidEmail", 0, time() + 86400, "/");
+setcookie("acceptStudent", 0, time() + 86400, "/");
+setcookie("loginerror", 0, time() + 86400, "/");
+setcookie("incorrectInput", 0, time() + 86400, "/");
+setcookie("emptyFields", 0, time() + 86400, "/");
+
+
 
 	header('Location: index.php');
 ?>
